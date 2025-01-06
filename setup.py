6850b857bf26ccaa2a76d73014be3ac4aca54a9a
DhairyaRathi123 <152994761+DhairyaRathi123@users.noreply.github.com>
2025-01-06 10:44:37 +0530

    removed pyprojecttoml

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='gazedetector',
     version='0.1',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'flask',
         'flask-cors',
@@ -14,5 +15,15 @@ setup(
             'gazedetector = gazedetector.server:run_server',
         ],
     },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
 )
- 
